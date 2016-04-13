cb8945c42f19a93ed695f0a8dfc844baf5a7c87a
Adam Wilbert <adam.wilbert@gmail.com>
2016-04-13 13:07:20 -0700

    initial

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+import "net/http"
+
+
+func main() {
+    router := gin.Default()
+
+    // Query string parameters are parsed using the existing underlying request object.
+    // The request responds to a url matching:  /welcome?firstname=Jane&lastname=Doe
+
+    router.GET("/welcome", func(c *gin.Context) {
+        firstname := c.DefaultQuery("firstname", "Guest")
+        lastname := c.Query("lastname") // shortcut for c.Request.URL.Query().Get("lastname")
+
+        c.String(http.StatusOK, "Hello %s %s", firstname, lastname)
+    })
+
+    router.GET("/", func(c *gin.Context){
+    	c.String(http.StatusOK, "Welcome, please proceed to /welcome")
+    })
+
+
+    router.Run(":8080")
+}
