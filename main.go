80089f4341358214cb986cadef5078a24bb79afc
Adam Wilbert <adam.wilbert@gmail.com>
2016-04-13 13:27:26 -0700

    adding html template rendering

--- main.go
+++ main.go
@@ -1,27 +1,19 @@
 package main
 
-import "github.com/gin-gonic/gin"
-
-import "net/http"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 
 func main() {
     router := gin.Default()
-
-    // Query string parameters are parsed using the existing underlying request object.
-    // The request responds to a url matching:  /welcome?firstname=Jane&lastname=Doe
-
-    router.GET("/welcome", func(c *gin.Context) {
-        firstname := c.DefaultQuery("firstname", "Guest")
-        lastname := c.Query("lastname") // shortcut for c.Request.URL.Query().Get("lastname")
-
-        c.String(http.StatusOK, "Hello %s %s", firstname, lastname)
+    router.LoadHTMLGlob("templates/*")
+    //router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
+    router.GET("/index", func(c *gin.Context) {
+        c.HTML(http.StatusOK, "index.tmpl", gin.H{
+            "title": "Main website",
+        })
     })
-
-    router.GET("/", func(c *gin.Context){
-    	c.String(http.StatusOK, "Welcome, please proceed to /welcome")
-    })
-
-
     router.Run(":8080")
 }
