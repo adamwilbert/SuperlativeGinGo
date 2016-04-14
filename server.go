84636b646d402a90679180ff65769c9c1f63b9a6
Adam Wilbert <adam.wilbert@gmail.com>
2016-04-14 09:30:32 -0700

    adding hello world component

--- server.go
+++ server.go
@@ -37,16 +37,20 @@ func main() {
 
     router.LoadHTMLGlob("templates/*")
     //router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
-    router.GET("/", func(c *gin.Context) {
+    router.GET("/main", func(c *gin.Context) {
         c.HTML(http.StatusOK, "index.tmpl", gin.H{
             "title": "Main website",
         })
     })
 
     router.GET("/test", func(c *gin.Context) {
-    c.Redirect(http.StatusMovedPermanently, "http://www.google.com/")
+    	c.Redirect(http.StatusMovedPermanently, "http://www.google.com/")
 
+	})
 
-})
+	router.GET("/", func(c *gin.Context) {
+		c.String(200, "hello world\n")
+	})
+
     router.Run(":8080")
 }
