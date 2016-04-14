77bf2b002e023c90a73837643bd195e71ea92854
Adam Wilbert <adam.wilbert@gmail.com>
2016-04-13 18:36:18 -0700

    adding latency and status logger

--- main.go
+++ main.go
@@ -3,17 +3,50 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"log"
+	"time"
 )
+
+
+func Logger() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        t := time.Now()
+
+        // Set example variable
+        c.Set("example", "12345")
+
+        // before request
+
+        c.Next()
+
+        // after request
+        latency := time.Since(t)
+        log.Println("latency = ", latency)
+
+        // access the status we are sending
+        status := c.Writer.Status()
+        log.Println("statuscode = ", status)
+    }
+}
 
 
 func main() {
     router := gin.Default()
+    router.Use(Logger())
+
+
     router.LoadHTMLGlob("templates/*")
     //router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
-    router.GET("/index", func(c *gin.Context) {
+    router.GET("/", func(c *gin.Context) {
         c.HTML(http.StatusOK, "index.tmpl", gin.H{
             "title": "Main website",
         })
     })
+
+    router.GET("/test", func(c *gin.Context) {
+    c.Redirect(http.StatusMovedPermanently, "http://www.google.com/")
+
+
+})
     router.Run(":8080")
 }
