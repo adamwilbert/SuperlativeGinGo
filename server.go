55184f8a89db48e060bec2b37fd24c0149377ff0
Adam Wilbert <adam.wilbert@gmail.com>
2016-04-18 17:39:37 -0700

    removing gin

--- server.go
+++ server.go
@@ -1,33 +1,14 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
-
 )
 
-
-
-
 func main() {
-    router := gin.Default()
+    http.HandleFunc("/", rootHandler)
+    http.ListenAndServe(":8080", nil)
+}
 
-    router.LoadHTMLGlob("templates/*")
-    //router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
-    router.GET("/main", func(c *gin.Context) {
-        c.HTML(http.StatusOK, "index.tmpl", gin.H{
-            "title": "Main website",
-        })
-    })
-
-    router.GET("/test", func(c *gin.Context) {
-    	c.Redirect(http.StatusMovedPermanently, "http://www.google.com/")
-
-	})
-
-	router.GET("/", func(c *gin.Context) {
-		c.String(200, "hello world\n")
-	})
-
-    router.Run(":8080")
+func rootHandler(w http.ResponseWriter, req *http.Request) {
+      w.Write([]byte("Hello universe"))
 }
